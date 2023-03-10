1a07ce2bd042f2a3f84f633f2015529079971132
Johann Fischer <johann.fischer@nordicsemi.no>
2023-03-10 18:16:20 +0100

    drivers: eth_w5500: rework ETHERNET_CONFIG_TYPE_PROMISC_MODE setting
    The driver reads W5500_S0_MR register in set_config() implementation
    unconditionally. Rework set_config() implementation.
    
    Signed-off-by: Johann Fischer <johann.fischer@nordicsemi.no>

--- drivers/ethernet/eth_w5500.c
+++ drivers/ethernet/eth_w5500.c
@@ -340,30 +340,34 @@ static int w5500_set_config(const struct device *dev,
 			    enum ethernet_config_type type,
 			    const struct ethernet_config *config)
 {
-	uint8_t mode;
-	uint8_t mr = W5500_S0_MR_MF;
+	if (type == ETHERNET_CONFIG_TYPE_PROMISC_MODE) {
+		if (IS_ENABLED(CONFIG_NET_PROMISCUOUS_MODE)) {
+			uint8_t mr = W5500_S0_MR_MF;
+			uint8_t mode;
 
-	w5500_spi_read(dev, W5500_S0_MR, &mode, 1);
-	if (IS_ENABLED(CONFIG_NET_PROMISCUOUS_MODE) &&
-	    type == ETHERNET_CONFIG_TYPE_PROMISC_MODE) {
-		if (config->promisc_mode) {
-			if (!(mode & BIT(mr))) {
-				return -EALREADY;
+			w5500_spi_read(dev, W5500_S0_MR, &mode, 1);
+
+			if (config->promisc_mode) {
+				if (!(mode & BIT(mr))) {
+					return -EALREADY;
+				}
+
+				WRITE_BIT(mode, mr, 0);
+			} else {
+				if (mode & BIT(mr)) {
+					return -EALREADY;
+				}
+
+				WRITE_BIT(mode, mr, 1);
 			}
+
+			return w5500_spi_write(dev, W5500_S0_MR, &mode, 1);
 		}
 
-		/* clear */
-		WRITE_BIT(mode, mr, 0);
-	} else {
-		if (mode & BIT(mr)) {
-			return -EALREADY;
-		}
-
-		/* set */
-		WRITE_BIT(mode, mr, 1);
+		return -ENOTSUP;
 	}
 
-	return w5500_spi_write(dev, W5500_S0_MR, &mode, 1);
+	return -ENOTSUP;
 }
 
 static int w5500_hw_start(const struct device *dev)
