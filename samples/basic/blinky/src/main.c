5af1a6c6dcd95e3a030abe02bf7fb1ce7215b389
Johann Fischer <johann.fischer@nordicsemi.no>
2024-03-28 00:06:57 +0100

    samples: blinky: change sample for quick nrfs testing
    Change sample for quick nrfs testing.
    This also requires some NRFS-related Kconfig options to be enabled.
    
    Signed-off-by: Johann Fischer <johann.fischer@nordicsemi.no>

--- samples/basic/blinky/src/main.c
+++ samples/basic/blinky/src/main.c
@@ -7,12 +7,31 @@
 #include <stdio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
+#include <nrfs_backend_ipc_service.h>
+#include <nrfs_usb.h>
 
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS   1000
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
+
+static void usbhs_periph_handler(nrfs_usb_evt_t const *p_evt, void *context)
+{
+	switch (p_evt->type) {
+	case NRFS_USB_EVT_VBUS_STATUS_CHANGE:
+		printf("USBHS new status, pll_ok = %d vreg_ok = %d vbus_detected = %d",
+			p_evt->usbhspll_ok, p_evt->vregusb_ok, p_evt->vbus_detected);
+
+		break;
+	case NRFS_USB_EVT_REJECT:
+		printf("Request rejected");
+		break;
+	default:
+		printf("Unexpected event: 0x%x", p_evt->type);
+		break;
+	}
+}
 
 /*
  * A build error on this line means your board is unsupported.
@@ -22,6 +41,7 @@ static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 int main(void)
 {
+	nrfs_err_t status;
 	int ret;
 	bool led_state = true;
 
@@ -32,6 +52,17 @@ int main(void)
 	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
 	if (ret < 0) {
 		return 0;
+	}
+
+	ret = nrfs_backend_wait_for_connection(K_MSEC(1000));
+	if (ret) {
+		printf("NRFS backend connection timeout");
+		return ret;
+	}
+
+	status = nrfs_usb_init(usbhs_periph_handler);
+	if (status != NRFS_SUCCESS) {
+		printf("Failed to init nrfs USB service: %d", status);
 	}
 
 	while (1) {
